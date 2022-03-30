f5e78ddb5f219ef0c191e24bfea2c5397f9b48fc
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-30 14:50:08 +0800

    Mari kita coba

--- ZeboRobot/modules/helper_funcs/handlers.py
+++ ZeboRobot/modules/helper_funcs/handlers.py
@@ -1,6 +1,6 @@
-import EmikoRobot.modules.sql.blacklistusers_sql as sql
-from EmikoRobot import ALLOW_EXCL
-from EmikoRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
+import ZeboRobot.modules.sql.blacklistusers_sql as sql
+from ZeboRobot import ALLOW_EXCL
+from ZeboRobot import DEV_USERS, DRAGONS, DEMONS, TIGERS, WOLVES
 
 from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, RegexHandler, Filters
