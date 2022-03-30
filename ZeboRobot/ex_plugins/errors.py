f23f6f365cfbb927f8a042d4cc24d7b03a11b9ca
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-03-30 14:29:52 +0800

    Mari kita coba

--- ZeboRobot/ex_plugins/errors.py
+++ ZeboRobot/ex_plugins/errors.py
@@ -2,7 +2,7 @@ import sys
 import traceback
 from functools import wraps
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
-from Zeborobot import EVENT_LOGS, pbot as app
+from ZeboRobot import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
