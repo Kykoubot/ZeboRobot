b165e9258e5b55a06116dc2454fe8bed54b8c896
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 01:15:59 +0800

    y

--- ZeboRobot/modules/afk.py
+++ ZeboRobot/modules/afk.py
@@ -1,12 +1,12 @@
 import random, html
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import (
+from ZeboRobot import dispatcher
+from ZeboRobot.modules.disable import (
     DisableAbleCommandHandler,
     DisableAbleMessageHandler,
 )
-from EmikoRobot.modules.sql import afk_sql as sql
-from EmikoRobot.modules.users import get_user_id
+from ZeboRobot.modules.sql import afk_sql as sql
+from ZeboRobot.modules.users import get_user_id
 from telegram import MessageEntity, Update
 from telegram.error import BadRequest
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
