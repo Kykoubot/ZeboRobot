ad8bb24bce96fe014670e94cd119947eb772ec23
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 02:52:45 +0800

    y

--- ZeboRobot/utils/functions.py
+++ ZeboRobot/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from EmikoRobot import aiohttpsession as aiosession
-from EmikoRobot.ex_plugins.dbfunctions import start_restart_stage
-from EmikoRobot.utils.http import get, post
+from ZeboRobot import aiohttpsession as aiosession
+from ZeboRobot.ex_plugins.dbfunctions import start_restart_stage
+from ZeboRobot.utils.http import get, post
 
 
 async def restart(m: Message):
