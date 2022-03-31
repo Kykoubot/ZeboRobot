b3394e0278da023cb381718c6e51879974d6eb2f
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 01:37:25 +0800

    y

--- ZeboRobot/modules/backups.py
+++ ZeboRobot/modules/backups.py
@@ -5,23 +5,23 @@ from telegram import ParseMode, Message
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler
 
-import EmikoRobot.modules.sql.notes_sql as sql
-from EmikoRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
-from EmikoRobot.__main__ import DATA_IMPORT
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
-from EmikoRobot.modules.helper_funcs.alternate import typing_action
+import ZeboRobot.modules.sql.notes_sql as sql
+from ZeboRobot import dispatcher, LOGGER, OWNER_ID, JOIN_LOGGER, SUPPORT_CHAT
+from ZeboRobot.__main__ import DATA_IMPORT
+from ZeboRobot.modules.helper_funcs.chat_status import user_admin
+from ZeboRobot.modules.helper_funcs.alternate import typing_action
 
-# from EmikoRobot.modules.rules import get_rules
-import EmikoRobot.modules.sql.rules_sql as rulessql
+# from ZeboRobot.modules.rules import get_rules
+import ZeboRobot.modules.sql.rules_sql as rulessql
 
-# from EmikoRobot.modules.sql import warns_sql as warnssql
-import EmikoRobot.modules.sql.blacklist_sql as blacklistsql
-from EmikoRobot.modules.sql import disable_sql as disabledsql
+# from ZeboRobot.modules.sql import warns_sql as warnssql
+import ZeboRobot.modules.sql.blacklist_sql as blacklistsql
+from ZeboRobot.modules.sql import disable_sql as disabledsql
 
-# from EmikoRobot.modules.sql import cust_filters_sql as filtersql
-# import EmikoRobot.modules.sql.welcome_sql as welcsql
-import EmikoRobot.modules.sql.locks_sql as locksql
-from EmikoRobot.modules.connection import connected
+# from ZeboRobot.modules.sql import cust_filters_sql as filtersql
+# import ZeboRobot.modules.sql.welcome_sql as welcsql
+import ZeboRobot.modules.sql.locks_sql as locksql
+from ZeboRobot.modules.connection import connected
 
 
 @user_admin
