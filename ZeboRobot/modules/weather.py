ba1119b3284b57c2fd7b071d5190492135c1cc5e
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 17:46:47 +0800

    y

--- ZeboRobot/modules/weather.py
+++ ZeboRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from EmikoRobot import *
+from ZeboRobot import *
 
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
+from ZeboRobot import telethn as tbot
+from ZeboRobot.events import register
 
 
 async def is_register_admin(chat, user):
