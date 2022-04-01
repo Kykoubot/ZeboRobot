13cf8f56e52a51c53f69dfe9fc8a746e9ead6873
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 18:48:43 +0800

    y

--- ZeboRobot/modules/sed.py
+++ ZeboRobot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from EmikoRobot import LOGGER, dispatcher
-from EmikoRobot.modules.disable import DisableAbleMessageHandler
-from EmikoRobot.modules.helper_funcs.regex_helper import infinite_loop_check
+from ZeboRobot import LOGGER, dispatcher
+from ZeboRobot.modules.disable import DisableAbleMessageHandler
+from ZeboRobot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
