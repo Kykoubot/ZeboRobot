b52ea0b8591a0b9333c6abc825cefffe4826a7f9
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 18:46:58 +0800

    y

--- ZeboRobot/modules/shippering.py
+++ ZeboRobot/modules/shippering.py
@@ -1,6 +1,6 @@
-from EmikoRobot import pbot as app
-from EmikoRobot.utils.errors import capture_err
-from EmikoRobot.ex_plugins.dbfunctions import get_couple, save_couple
+from ZeboRobot import pbot as app
+from ZeboRobot.utils.errors import capture_err
+from ZeboRobot.ex_plugins.dbfunctions import get_couple, save_couple
 from pyrogram import filters
 import random
 from datetime import datetime
