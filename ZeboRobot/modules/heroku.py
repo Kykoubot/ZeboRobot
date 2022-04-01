9214e03c6ba45ccc84c27c99564b4c5f208e3359
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 18:43:31 +0800

    y

--- ZeboRobot/modules/heroku.py
+++ ZeboRobot/modules/heroku.py
@@ -5,8 +5,8 @@ import os
 import heroku3
 import requests
 
-from EmikoRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
-from EmikoRobot.events import register
+from ZeboRobot import telethn as borg, HEROKU_APP_NAME, HEROKU_API_KEY, OWNER_ID
+from ZeboRobot.events import register
 
 heroku_api = "https://api.heroku.com"
 Heroku = heroku3.from_key(HEROKU_API_KEY)
