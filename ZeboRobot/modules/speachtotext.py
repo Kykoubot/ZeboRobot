af539c926562f319c35b48272a434d81752bffad
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 18:45:13 +0800

    y

--- ZeboRobot/modules/speachtotext.py
+++ ZeboRobot/modules/speachtotext.py
@@ -10,9 +10,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from EmikoRobot import *
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from ZeboRobot import *
+from ZeboRobot.events import register
+from ZeboRobot import telethn as tbot
 
 
 async def is_register_admin(chat, user):
