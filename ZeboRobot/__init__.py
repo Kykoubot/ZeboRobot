bdd7940d1bb5f6d542e9a7657f8042a5c46272fb
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 14:28:24 +0800

    y

--- ZeboRobot/__init__.py
+++ ZeboRobot/__init__.py
@@ -24,7 +24,7 @@ StartTime = time.time()
 
 
 def get_user_list(__init__, key):
-    with open("{}/EmikoRobot/{}".format(os.getcwd(), __init__), "r") as json_file:
+    with open("{}/ZeboRobot/{}".format(os.getcwd(), __init__), "r") as json_file:
         return json.load(json_file)[key]
 
 
