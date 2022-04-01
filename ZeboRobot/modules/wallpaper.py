7d5598fd863551f44433e59ea74ec94c16c91792
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-01 17:55:25 +0800

    y

--- ZeboRobot/modules/wallpaper.py
+++ ZeboRobot/modules/wallpaper.py
@@ -1,8 +1,8 @@
 from random import randint
 
 import requests as r
-from EmikoRobot import SUPPORT_CHAT, WALL_API, dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from ZeboRobot import SUPPORT_CHAT, WALL_API, dispatcher
+from ZeboRobot.modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext
 
