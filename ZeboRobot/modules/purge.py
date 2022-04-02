2c03543043a608363ad822d8261103e31e17e99f
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-02 08:50:35 +0800

    y

--- ZeboRobot/modules/purge.py
+++ ZeboRobot/modules/purge.py
@@ -2,8 +2,8 @@ import asyncio
 import time
 from telethon import events
 
-from EmikoRobot import telethn
-from EmikoRobot.modules.helper_funcs.telethn.chatstatus import (
+from ZeboRobot import telethn
+from ZeboRobot.modules.helper_funcs.telethn.chatstatus import (
     can_delete_messages,
     user_is_admin,
 )
