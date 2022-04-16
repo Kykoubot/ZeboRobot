0378af8232f26aeb4d9d413fdac1b6deb2053fcb
ZeaFeya <99448657+Kykoubot@users.noreply.github.com>
2022-04-17 01:49:11 +0800

    Update misc.py

--- ZeboRobot/modules/misc.py
+++ ZeboRobot/modules/misc.py
@@ -35,14 +35,14 @@ MARKDOWN_HELP = f"""
 Markdown is a very powerful formatting tool supported by telegram. {dispatcher.bot.first_name} has some enhancements, to make sure that \
 saved messages are correctly parsed, and to allow you to create buttons.
 
-➢ <code>_italic_</code>: wrapping text with '_' will produce italic text
-➢ <code>*bold*</code>: wrapping text with '*' will produce bold text
-➢ <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
-➢ <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
+⏣ <code>_italic_</code>: wrapping text with '_' will produce italic text
+⏣ <code>*bold*</code>: wrapping text with '*' will produce bold text
+⏣ <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
+⏣ <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
 and tapping on it will open the page at <code>someURL</code>.
 <b>Example:</b><code>[test](example.com)</code>
 
-➢ <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
+⏣ <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
 buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
 will be the url which is opened.
 <b>Example:</b> <code>[This is a button](buttonurl:example.com)</code>
@@ -110,7 +110,7 @@ def wiki(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Enter keywords!")
     else:
         try:
-            pertama = update.effective_message.reply_text("🔁 Loading...")
+            pertama = update.effective_message.reply_text("🔄 Loading...")
             keyboard = InlineKeyboardMarkup(
                 [
                     [
@@ -184,14 +184,14 @@ def wall(update: Update, context: CallbackContext):
 __help__ = """
 *Available commands:*
 
-➢ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
-➢ /paste*:* Saves replied content to `nekobin.com` and replies with a url
-➢ /react*:* Reacts with a random reaction 
-➢ /ud <word>*:* Type the word or expression you want to search use
-➢ /reverse*:* Does a reverse image search of the media which it was replied to.
-➢ /wiki <query>*:* wikipedia your query
-➢ /wall <query>*:* get a wallpaper from wall.alphacoders.com
-➢ /cash*:* currency converter
+⏣ /markdownhelp*:* quick summary of how markdown works in telegram - can only be called in private chats
+⏣ /paste*:* Saves replied content to `nekobin.com` and replies with a url
+⏣ /react*:* Reacts with a random reaction 
+⏣ /ud <word>*:* Type the word or expression you want to search use
+⏣ /reverse*:* Does a reverse image search of the media which it was replied to.
+⏣ /wiki <query>*:* wikipedia your query
+⏣ /wall <query>*:* get a wallpaper from wall.alphacoders.com
+⏣ /cash*:* currency converter
  Example:
  `/cash 1 USD INR`  
       _OR_
@@ -200,10 +200,9 @@ __help__ = """
 """
 
 ECHO_HANDLER = DisableAbleCommandHandler(
-    "echo", echo, filters=Filters.chat_type.groups, run_async=True
-)
+    "echo", echo, filters=Filters.chat_type.groups, run_async=True)
 MD_HELP_HANDLER = CommandHandler("markdownhelp", markdown_help, run_async=True)
-WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki)
+WIKI_HANDLER = DisableAbleCommandHandler("wiki", wiki, run_async=True)
 WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall, run_async=True)
 
 dispatcher.add_handler(ECHO_HANDLER)
